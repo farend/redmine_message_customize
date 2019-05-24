1f3ecb1729ffa38959ec90ab455d8728713f4acf
ishikawa999 <remember929@gmail.com>
2019-05-24 16:09:22 +0900

    Add test_toggle_enabled_except_admin_user

--- test/functional/custom_message_settings_controller_test.rb
+++ test/functional/custom_message_settings_controller_test.rb
@@ -24,8 +24,7 @@ class CustomMessageSettingsControllerTest < Redmine::ControllerTest
   def test_edit_except_admin_user
     @request.session[:user_id] = 2
     get :edit
-    assert_response 403
-    assert_select 'p#errorExplanation', text: 'You are not authorized to access this page.'
+    assert_redirected_to (/#{signin_path}/)
   end
 
   def test_update_with_custom_messages
@@ -53,12 +52,11 @@ class CustomMessageSettingsControllerTest < Redmine::ControllerTest
     assert_select 'h2', :text => l(:label_custom_messages)
     assert_select 'div#errorExplanation'
   end
-  def test_edit_except_admin_user
+  def test_update_except_admin_user
     @request.session[:user_id] = 2
     get :update, params: { settings: {'custom_messages'=>{'label_home' => 'Home3'}}, lang: 'en', tab: 'normal' }
 
-    assert_response 403
-    assert_select 'p#errorExplanation', text: 'You are not authorized to access this page.'
+    assert_redirected_to (/#{signin_path}/)
   end
 
   def test_toggle_enabled
@@ -68,5 +66,11 @@ class CustomMessageSettingsControllerTest < Redmine::ControllerTest
 
     patch :toggle_enabled
     assert_equal l(:notice_enabled_customize), flash[:notice]
+  end
+  def test_toggle_enabled_except_admin_user
+    @request.session[:user_id] = 2
+    patch :toggle_enabled
+
+    assert_redirected_to (/#{signin_path}/)
   end
 end
